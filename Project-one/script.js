89c607e635c7de314c0695b91da0fa0eda53bed6
Ayodara200 <Daramola.A@Bullseyemanpower.com>
2023-06-10 14:39:22 +0100

    my project

--- Project-one/script.js
+++ Project-one/script.js
@@ -1,18 +1,7 @@
-// Get the necessary elements from the DOM
-const toggleSwitch = document.getElementById('toggle-switch');
-const monthlyPlan = document.querySelector('.plan.monthly');
-const annualPlan = document.querySelector('.plan.annual');
+const toggle = document.querySelector('.toggle')
+const main = document.querySelector('main')
 
-
-// Add event listener to the toggle switch
-toggleSwitch.addEventListener('change', () => {
-  if (toggleSwitch.checked) {
-    // Hide monthly plan, show annual plan
-    monthlyPlan.style.display = 'none';
-    annualPlan.style.display = 'block';
-  } else {
-    // Show monthly plan, hide annual plan
-    monthlyPlan.style.display = 'block';
-    annualPlan.style.display = 'none';
-  }
-});
+toggle.addEventListener('click',()=>{
+    toggle.classList.toggle('active')
+    main.classList.toggle('active')
+})
